8f8d4e8b11e9ce09cda756f975dbc347c1b70d3a
Constantin Vigulear <constantin.vigulear@iongroup.com>
2023-10-25 18:42:27 +0300

    Refactor

--- src/main/java/com/vigulear/spring_batch/repository/OverseasTradeIndexesRepository.java
+++ src/main/java/com/vigulear/spring_batch/repository/OverseasTradeIndexesRepository.java
@@ -1,7 +1,6 @@
 package com.vigulear.spring_batch.repository;
 
 import com.vigulear.spring_batch.entity.OverseasTradeIndexes;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 /**
