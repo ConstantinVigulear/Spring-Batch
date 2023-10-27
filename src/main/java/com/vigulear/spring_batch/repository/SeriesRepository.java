1f98c9cd0ce19634ef9675040ad7fa3a4544139e
Constantin Vigulear <constantin.vigulear@iongroup.com>
2023-10-27 15:31:38 +0300

    Refactor

--- src/main/java/com/vigulear/spring_batch/repository/SeriesRepository.java
+++ src/main/java/com/vigulear/spring_batch/repository/SeriesRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 /**
  * @author Constantin Vigulear
  */
+@SuppressWarnings("unused")
 public interface SeriesRepository
     extends CrudRepository<Series, Long>,
         PagingAndSortingRepository<Series, Long> {}
